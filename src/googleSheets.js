165487d70c751e2fe71fd4cfeaa7b073c929e317
Sanjay Jawahar <balasanjaykumar@gmail.com>
2026-02-20 00:03:21 +0530

    Test

--- src/googleSheets.js
+++ src/googleSheets.js
@@ -33,6 +33,7 @@ const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
  * Create an authenticated Google Sheets client using the service account.
  */
 function getSheetsClient() {
+    console.log(serviceAccount.private_key)
   const auth = new google.auth.JWT(
     serviceAccount.client_email,
     null,
